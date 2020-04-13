8b1ad7a515bc5d8344a18c7e704e544f53093c41
Alexis CADD <62930592+ke100@users.noreply.github.com>
2020-04-13 22:49:18 +0200

    Add files via upload

---
+++ lib/05_timer.rb
@@ -0,0 +1,7 @@
+  def time_string total_seconds
+  hours = total_seconds / (60 * 60)
+  minutes = (total_seconds / 60) % 60
+  seconds = total_seconds % 60
+
+  "#{ hours } h #{ minutes } m #{ seconds } s"
+end
